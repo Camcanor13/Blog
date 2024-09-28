268a5215afc1008a78b8186474d4c4d9255f54d2
LuzfernandezG <fernandezluz1995@gmail.com>
2024-09-27 22:24:23 -0500

    AHI ESTA :) TODO SUPUESTAMENTE

--- Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54e7c2e30f03260746172ae96acac99d8f2a985c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6452662262d842afea72b295dca18eb52a2c34c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
